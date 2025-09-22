cdaa60d42f0a4d53b8aaf1dbf06dd951dcd66c23
Gabriel-Lecoeur <gabriel.lecoeur@edu.esiee.fr>
2025-09-22 11:24:05 +0000

    04-primes

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from math import sqrt
-
-#### Fonction secondaire
-
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
+    if p<2:
+        return False  
+    for i in range (2,p):
+        if p%i==0:
+            return False
+    return True
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
 
     for n in range(100):
         if isprime(n):
